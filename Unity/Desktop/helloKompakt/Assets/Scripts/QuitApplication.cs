da24d721f3396d050369eff6d99495dda10de741
Manfred Brill <manfred.brill@hs-kl.de>
2019-08-29 14:22:58 +0200

    helloKompakt abgeschlossen.
    Build konfiguriert und durchgeführt.
    
    Build und Player Settings
    
    QuitApplication mit Stop in Build und im Editor
    
    Verbindung zwischen Script und GameObject
    
    Eigenes Package zum Wiederverwenden angelegt.

---
+++ Unity/Desktop/helloKompakt/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Erstes Beispiel einer C# Klasse in einer Unity-Anwendung
+/// </summary>
+public class QuitApplication : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+            // Esc is ignored in Editor playback mode
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+    }
+}
